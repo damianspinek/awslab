150e05509d08b2a7c1d2136f581e47c47864bf76
Damian Spinek <damian00737@gmail.com>
2016-04-12 11:14:31 +0200

    Create instance

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var lab1_1 = require("./lab/lab1_1").lab;
 var example_1 = require("./lab/example_1").lab
 var ec2instance = require("./lab/ec2instance").info;
+var create = require("./lab/create").lab;
 var AWS = require('aws-sdk');
 
 var PORT = 8080;
@@ -9,7 +10,8 @@ var urlMap = [
 	{path: "/", action:__dirname + "/static/index.html"},	 
 	{path: "/digest", action: lab1_1},	
 	{path: "/example_1", action: example_1}, 
-	{path: "/ec2instance", action: ec2instance}
+	{path: "/ec2instance", action: ec2instance},
+	{path: "/create", action: create}
 	];
 
 AWS.config.loadFromPath('./config.json');
