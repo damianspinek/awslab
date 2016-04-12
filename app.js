75f97243aaf2bb8f5e4416a3226ff7929c2ae826
Damian Spinek <damian00737@gmail.com>
2016-04-12 08:32:36 +0000

    point 2

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
-var lab1_1 = require("./lab/lab1_1").lab
-var example_1 = require("./example_1").lab;
+var lab1_1 = require("./lab/lab1_1").lab;
+var example_1 = require("./lab/example_1").lab
+var AWS = require('aws-sdk');
 
 var PORT = 8080;
-
 
 var urlMap = [
 	{path: "/", action:__dirname + "/static/index.html"},	 
 	{path: "/digest", action: lab1_1},	
 	{path: "/example_1", action: example_1}, 
 	];
+
+AWS.config.loadFromPath('./config.json');
 
 var service = require("./lib/service").http(urlMap);
 
