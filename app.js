07ac3d2e39c196a46772776d916829fc45ea01fd
Damian Spinek <damian00737@gmail.com>
2016-04-12 10:55:04 +0200

    get insance info

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var lab1_1 = require("./lab/lab1_1").lab;
 var example_1 = require("./lab/example_1").lab
+var lab2 = require("./lab/ec2instance").info;
 var AWS = require('aws-sdk');
 
 var PORT = 8080;
@@ -8,6 +9,7 @@ var urlMap = [
 	{path: "/", action:__dirname + "/static/index.html"},	 
 	{path: "/digest", action: lab1_1},	
 	{path: "/example_1", action: example_1}, 
+	{path: "/ec2instance", action: ec2instance}
 	];
 
 AWS.config.loadFromPath('./config.json');
